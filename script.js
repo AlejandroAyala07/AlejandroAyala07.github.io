7079de2da9168850f02e6a80f3fcc8a510be4f7a
Alejandro Ayala <105084097+AlejandroAyala07@users.noreply.github.com>
2024-02-28 11:16:10 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/worker/service-worker.js')
-      .then(reg => console.log('Registro de SW exitoso', reg))
-      .catch(err => console.warn('Error al tratar de registrar el service-worker', err))
-  }
